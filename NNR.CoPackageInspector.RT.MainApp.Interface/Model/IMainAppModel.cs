319ae3a1fccb46eb1ddc005b5b71379ecbd9e2a6
Takayoshi Nakagawa <takayoshi@tk-nkgw.jp>
2023-12-14 22:24:00 +0900

    MainAppModelのインターフェースを修正しました。

--- NNR.CoPackageInspector.RT.MainApp.Interface/Model/IMainAppModel.cs
+++ NNR.CoPackageInspector.RT.MainApp.Interface/Model/IMainAppModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,6 @@ namespace NNR.CoPackageInspector.RT.MainApp.Interface.Model
 {
     public interface IMainAppModel
     {
+        IApplicationCondition AppCondition { get; }
     }
 }
