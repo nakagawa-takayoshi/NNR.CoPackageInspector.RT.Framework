20d80ee80a8f97147f37d8e394fa07acd8b0ec77
Takayoshi Nakagawa <takayoshi@tk-nkgw.jp>
2023-12-14 22:19:12 +0900

    アプリケーションコンディションクラスを追加しました。

---
+++ NNR.CoPackageInspector.RT.MainApp.Interface/Model/Enums/ApplicationConditionState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNR.CoPackageInspector.RT.MainApp.Interface.Model.Enums
+{
+    public enum ApplicationConditionState
+    {
+        Startup = 0,
+        HardwareInitialize,
+        Running,
+        Exit,
+        End
+    }
+}
---
+++ NNR.CoPackageInspector.RT.MainApp.Interface/Model/IApplicationCondition.cs
@@ -0,0 +1,14 @@
+﻿using NNR.CoPackageInspector.RT.MainApp.Interface.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNR.CoPackageInspector.RT.MainApp.Interface.Model
+{
+    public interface IApplicationCondition
+    {
+        ApplicationConditionState State { get; set; }
+    }
+}
