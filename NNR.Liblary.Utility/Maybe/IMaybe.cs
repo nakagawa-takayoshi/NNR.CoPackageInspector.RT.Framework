4504b5788626c727cda7474e6272a35da5f35055
Takayoshi Nakagawa <takayoshi@tk-nkgw.jp>
2023-12-14 22:49:08 +0900

    Maybeクラスを追加しました。

---
+++ NNR.Liblary.Utility/Maybe/IMaybe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNR.Liblary.Utility.Maybe
+{
+    public interface IMaybe<T>
+    {
+        bool HasObject { get; }
+
+        T Object { get; }
+    }
+}
---
+++ NNR.Liblary.Utility/Maybe/Maybe.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNR.Liblary.Utility.Maybe
+{
+    public class Maybe
+    {
+        private class EmptyObject<T> : IMaybe<T>
+        {
+            public bool HasObject => false;
+
+            public T Object
+            {
+                get { throw new InvalidOperationException(@"このオブジェクトは空オブジェクトです"); }
+            }
+
+        }
+
+
+        private class Something<T> : IMaybe<T>
+        {
+            private T _object;
+
+            public Something(T obj)
+            {
+                _object = obj;
+            }
+
+            public bool HasObject => true;
+
+            public T Object => _object;
+        }
+
+
+        /// <summary>
+        /// 空オブジェクトを取得します。
+        /// </summary>
+        public static IMaybe<T> Empty<T>()
+        {
+            return new EmptyObject<T>();
+        }
+
+        public static IMaybe<T> Create<T>(T obj)
+        {
+            return new Something<T>(obj);
+        }
+    }
+}
