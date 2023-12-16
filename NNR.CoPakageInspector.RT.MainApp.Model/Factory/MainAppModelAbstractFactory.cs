d9ef4968550a22dd6b2e814a29fdd3525afce942
Takayoshi Nakagawa <takayoshi@tk-nkgw.jp>
2023-12-16 11:57:44 +0900

    MainAppModelAbstractFactoryクラスを移動しました。

---
+++ NNR.CoPakageInspector.RT.MainApp.Model/Factory/MainAppModelAbstractFactory.cs
@@ -0,0 +1,43 @@
+﻿using NNR.CoPakageInspector.RT.MainApp.Model.Station;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNR.CoPakageInspector.RT.MainApp.Model.Factory
+{
+    public class MainAppModelAbstractFactory
+    {
+        MainAppModelFactory _factory = new MainAppModelFactory();
+
+        public static MainAppModelAbstractFactory Create()
+        {
+            return new MainAppModelAbstractFactory();
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        private MainAppModelAbstractFactory()
+        {
+        }
+
+        /// <summary>
+        /// ステーション群を追加します。
+        /// </summary>
+        public void Add(StationCollection stations)
+        {
+            _factory.StationCollection = stations;
+        }
+
+        /// <summary>
+        /// MainAppModelのファクトリークラスを生成します。
+        /// </summary>
+        public MainAppModelFactory CreteFactory()
+        {
+            return _factory;
+        }
+
+    }
+}
