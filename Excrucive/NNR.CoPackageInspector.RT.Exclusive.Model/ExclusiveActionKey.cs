46c6406b778992f4342c6ce6eb2b6cb54e6bb565
Takayoshi Nakagawa <takayoshi@tk-nkgw.jp>
2024-05-25 18:34:22 +0900

    排他制御プロジェクトを追加しました。

---
+++ Excrucive/NNR.CoPackageInspector.RT.Exclusive.Model/ExclusiveActionKey.cs
@@ -0,0 +1,13 @@
+﻿using NNR.CoPackageInspector.RT.Exclusive.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNR.CoPackageInspector.RT.Exclusive.Model
+{
+    public class ExclusiveActionKey : IActionKey
+    {
+    }
+}
