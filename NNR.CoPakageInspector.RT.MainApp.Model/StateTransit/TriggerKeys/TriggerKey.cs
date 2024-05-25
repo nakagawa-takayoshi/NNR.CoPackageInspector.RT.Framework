b5d25ac837db4bf757aae2d5b10eaaf9af807591
Takayoshi Nakagawa <takayoshi@tk-nkgw.jp>
2024-05-25 16:28:55 +0900

    サンプルのトリガーキーを追加しました。

---
+++ NNR.CoPakageInspector.RT.MainApp.Model/StateTransit/TriggerKeys/TriggerKey.cs
@@ -0,0 +1,14 @@
+﻿using NNR.CoPackageInspector.RT.Framework.Model.StateTansit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNR.CoPackageInspector.RT.MainApp.Model.StateTransit.TriggerKeys
+{
+    public static class TriggerKey
+    {
+        public static TriggerKeyModel SampleTriggerKey { get; } = new TriggerKeyModel();
+    }
+}
