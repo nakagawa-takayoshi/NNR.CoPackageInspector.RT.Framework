6e2fa9ea1ace43a31deda96d16623345fb14536b
Takayoshi Nakagawa <takayoshi@tk-nkgw.jp>
2024-05-25 18:35:57 +0900

    ステータス遷移制御機能を追加しました。

---
+++ NNR.CopackageInspector.RT.Framework.Controller/StateTransition/StateTransitionObsavable.cs
@@ -0,0 +1,38 @@
+﻿using NNR.CoPackageInspector.RT.Framework.Interface.Model.StateTansit;
+using NNR.CoPackageInspector.RT.Framework.Model.StateTansit;
+using NNR.CoPackageInspector.RT.MainApp.Model.StateTransit.TriggerKeys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNR.CopackageInspector.RT.Framework.Controller.StateTransition
+{
+    /// <summary>
+    /// 状態遷移の監視コントローラー
+    /// </summary>
+    public class StateTransitionObsavable : IStateTransitionObsavable
+    {
+
+        private ITriggerKey _triggerKeyModel;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public StateTransitionObsavable(ITriggerKey triggerKeyModel)
+        {
+            _triggerKeyModel = triggerKeyModel;
+        }
+
+        /// <summary>
+        /// 購読
+        /// </summary>
+        public IDisposable Subscribe(Action action)
+        {
+            _triggerKeyModel.Triggered += action;
+            return new TriggerKeyUnscriber(_triggerKeyModel, action);
+        }
+    }
+}
---
+++ NNR.CopackageInspector.RT.Framework.Controller/TriggerKeyController.cs
@@ -0,0 +1,18 @@
+﻿using NNR.CoPackageInspector.RT.Framework.Interface.Model.StateTansit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNR.CopackageInspector.RT.Framework.Controller
+{
+    public static class TriggerKeyController
+    {
+        public static void Trigger(ITriggerKey triggerKey)
+        {
+            triggerKey.TriggerHandle.Set();
+        }
+    }
+}
